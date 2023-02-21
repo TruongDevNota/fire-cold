c599181f82ec5fdda87f9e5cb38f3b29711434db
Dinh Quang Hieu <hieudq@yunero.io>
2023-02-21 18:42:04 +0700

    GameItem data asset, Init Items

---
+++ mihn_GoodsMatch/Assets/GameCore/ModBartender/GameData/GameItemAsset.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using MyBox;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "GameItemsAsset", menuName = "DataAsset/GameItemAsset")]
+public class GameItemAsset : ScriptableObject
+{
+    [SerializeField] List<ItemDatum> list;
+    [SerializeField] List<Goods_Item> itemModels;
+
+    public List<ItemDatum> unlockedList
+    {
+        get
+        {
+            return list?.Where(x => x.unlocked).ToList();
+        }
+    }
+    public List<ItemDatum> itemSaveList
+    {
+        get => list?.Where(x => x.unlocked)
+            .Select(x => new ItemDatum { id = x.id, unlocked = x.unlocked }).ToList();
+    }
+    public ItemDatum GetItemDatumById(string id)
+    {
+        return list?.FirstOrDefault(x => string.Compare(x.id, id) == 0);
+    }
+    public ItemDatum GetDefinitionByType(eItemType type)
+    {
+        return list?.FirstOrDefault(x => x.itemProp.Type == type);
+    }
+
+    [ButtonMethod]
+    public void ResetData()
+    {
+        list.Clear();
+
+        for (int i = 0; i < itemModels.Count; i++)
+        {
+            var datum = new ItemDatum()
+            {
+                id = itemModels[i].name.ToLower(),
+                unlocked = i < 18,
+                itemProp = itemModels[i],
+            };
+            list.Add(datum);
+        }
+#if UNITY_EDITOR
+        EditorUtility.SetDirty(this);
+#endif
+    }
+}
+
+[System.Serializable]
+public class ItemDatum
+{
+    public string id;
+    public bool unlocked;
+    public Goods_Item itemProp;
+}
---
+++ mihn_GoodsMatch/Assets/GameCore/ModBartender/Scripts/BoardGame_Bartender.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using Random = UnityEngine.Random;
+
+public class BoardGame_Bartender : MonoBehaviour
+{
+    [SerializeField] GameItemAsset gameItemAsset;
+
+    private bool isAlert = false;
+
+    [Header("Draging")]
+    [SerializeField] public Vector3 touchPositionOffset;
+    private Goods_Item dragingItem = null;
+    private bool isDraggingItem = false;
+
+    [Header("Items List")]
+    public List<Goods_Item> items = new List<Goods_Item>();
+    public List<ShelfUnit> shelves = new List<ShelfUnit>();
+    private List<eItemType> typeGroup1 = new List<eItemType>();
+    private List<eItemType> typeGroup2 = new List<eItemType>();
+    private List<eItemType> typpGroup3 = new List<eItemType>();
+    private List<eItemType> typeGroup4 = new List<eItemType>();
+
+    private void Start()
+    {
+        StartCoroutine(YieldInit(0));
+    }
+
+    private IEnumerator YieldInit(int level)
+    {
+        int emptyCount = 0;
+        int startShelfIndex = UnityEngine.Random.Range(0, shelves.Count);
+
+        foreach(var shelf in shelves)
+            shelf.InitCells();
+        yield return new WaitForEndOfFrame();
+        typeGroup1.Clear();
+        typeGroup2.Clear();
+        typpGroup3.Clear();
+        typeGroup4.Clear();
+        foreach (var item in items)
+            item.Recycle();
+        items.Clear();
+        yield return new WaitForEndOfFrame();
+        var allItemUnlocked = NextList(gameItemAsset.unlockedList);
+        InitItems((List<ItemDatum>)allItemUnlocked);
+        yield return new WaitForEndOfFrame();
+
+        for(int i = 0; i < items.Count; i++)
+        {
+            if(emptyCount < 3)
+            {
+                int emptyChange = Random.Range(0, 100);
+                if(emptyChange < 10)
+                {
+                    emptyCount++;
+                    startShelfIndex += 2;
+                }
+            }
+
+            var index = shelves[startShelfIndex % shelves.Count].CheckItemFitOnShelf(1, 0);
+            while (index < 0)
+            {
+                startShelfIndex += 2;
+                index = shelves[startShelfIndex % shelves.Count].CheckItemFitOnShelf(1, 0);
+            }
+
+            var newItem = items[i].Spawn();
+            newItem.pFirstLeftCellIndex = index;
+            shelves[startShelfIndex % shelves.Count].DoPutNewItem(newItem);
+            newItem.transform.parent = shelves[startShelfIndex % shelves.Count].transform;
+            startShelfIndex += 2;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private void InitItems(List<ItemDatum> allItemUnlocked)
+    {
+        for (int i = 0; i < allItemUnlocked.Count; i++)
+        {
+            if (i < 4)
+            {
+                typeGroup1.Add(allItemUnlocked[i].itemProp.Type);
+                AddItemsToList(allItemUnlocked[i].itemProp, 3);
+            }
+            else if (i < 8)
+            {
+                typeGroup2.Add(allItemUnlocked[i].itemProp.Type);
+                AddItemsToList(allItemUnlocked[i].itemProp, 2);
+            }
+            else if (i < 12)
+            {
+                typpGroup3.Add(allItemUnlocked[i].itemProp.Type);
+                AddItemsToList(allItemUnlocked[i].itemProp, 1);
+            }
+            else
+                typeGroup4.Add(allItemUnlocked[i].itemProp.Type);
+        }
+    }
+    private void AddItemsToList(Goods_Item item, int amount = 1)
+    {
+        for (int i = 0; i < amount; i++)
+            items.Add(item);
+    }
+
+    public static IList<T> NextList<T>(IEnumerable<T> source)
+    {
+        System.Random r = new System.Random();
+        var list = new List<T>();
+        foreach (var item in source)
+        {
+            var i = r.Next(list.Count + 1);
+            if (i == list.Count)
+            {
+                list.Add(item);
+            }
+            else
+            {
+                var temp = list[i];
+                list[i] = item;
+                list.Add(temp);
+            }
+        }
+        return list;
+    }
+}
